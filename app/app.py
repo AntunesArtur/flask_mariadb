3b8467f487cf4d67574b0ccdd236dd0239d8e2ee
AntunesArtur <arturantunes25@hotmail.com>
2024-09-25 19:00:59 +0100

    adicionar swagger

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flasgger import Swagger
 import mysql.connector
 import os
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 # Função para conectar ao banco de dados
 def get_db_connection():
@@ -20,9 +22,29 @@ def get_db_connection():
 def index():
     return jsonify({'message': 'Welcome to the Flask APP!'})
 
-# Rota para adicionar um usuário no banco de dados
-
+@app.route('/users', methods=['POST'])
 def add_user():
+    """
+    Adiciona um usuário ao banco de dados.
+    ---
+    parameters:
+      - name: user
+        in: body
+        required: true
+        schema:
+          id: User
+          required:
+            - name
+          properties:
+            name:
+              type: string
+              description: Nome do usuário
+    responses:
+      201:
+        description: Usuário adicionado com sucesso.
+      400:
+        description: Erro de validação.
+    """
     data = request.json
     name = data['name']
     
@@ -35,11 +57,27 @@ def add_user():
     cursor.close()
     conn.close()
     
-    return jsonify({'message': 'User added successfully!'})
+    return jsonify({'message': 'User added successfully!'}), 201
 
-# Rota para listar todos os usuários
 @app.route('/users', methods=['GET'])
 def get_users():
+    """
+    Lista todos os usuários do banco de dados.
+    ---
+    responses:
+      200:
+        description: Lista de usuários.
+        schema:
+          type: array
+          items:
+            properties:
+              id:
+                type: integer
+                description: ID do usuário.
+              name:
+                type: string
+                description: Nome do usuário.
+    """
     conn = get_db_connection()
     cursor = conn.cursor()
     
